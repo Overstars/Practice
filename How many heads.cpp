78429eb6e556e73da2b8b844f772faae2acf1180
Overstars <42737768+Overstars@users.noreply.github.com>
2018-10-02 20:47:36 +0800

    Add files via upload

---
+++ How many heads.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+	int heads;
+	
+	printf("How many heads do you have?\n");
+	scanf("%d", &heads);
+	printf("So you have %d head(s)!\n",heads);
+	
+	return 0;
+}
