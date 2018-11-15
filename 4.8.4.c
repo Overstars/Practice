7576310bb90ebfbd4adad285708f9c7bd62a19eb
Overstars <42737768+Overstars@users.noreply.github.com>
2018-11-14 18:44:44 -0600

    Add files via upload

---
+++ 4.8.4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+	float height;
+	printf("Please enter your height in CM.\n");
+	scanf("%f",&height);
+	printf("Dabney,you are %f meter(s) tall.\n",height/100);
+	
+	return 0;
+}
