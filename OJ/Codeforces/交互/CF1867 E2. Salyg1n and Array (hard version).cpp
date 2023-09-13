6dfece3113c4dee82c653da609f23f998b791196
Overstars <1635396212@qq.com>
2023-09-13 18:58:12 +0800

    [update]CD1967E2没写完

---
+++ OJ/Codeforces/交互/CF1867 E2. Salyg1n and Array (hard version).cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn = 1e5 + 10;
+/*
+ *
+ */
+int main()
+{
+	int t, n, k, ans = 0;
+	std::ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	cin >> t >> n >> k;
+	while (t--)
+	{
+		cout << "";
+		cout.flush();
+		cin >> 
+	}
+	cout << "! " << ans << endl;
+	return 0;
+}
