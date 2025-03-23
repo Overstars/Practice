ab53f8c9448f1f46776ba95fa8c05aed1770a039
Overstars <1635396212@qq.com>
2025-03-23 13:23:01 +0800

    [add]移动java并添加Kotlin

--- MyKotlin/src/main/kotlin/2116. 判断一个括号字符串是否有效.kt
+++ MyKotlin/src/main/kotlin/2116. 判断一个括号字符串是否有效.kt
@@ -1,12 +1,38 @@
-/*
- * locked : 1锁定，0可变
-
- */
 fun canBeValid(s: String, locked: String): Boolean {
     var ans = true
-    var cnt = 0
+    var total = 0
+    var left = 0
+    var right = 0
     val len = s.length
-
+    if (len % 2 == 1)
+        ans = false
+    for (i in 0 until len) {
+        if (locked[i] == '0')
+            total++
+        else {
+            if (s[i] == '(')
+                left++
+            else
+                right++
+        }
+        if (left + total < right)
+            ans = false
+    }
+    left = 0
+    right = 0
+    total = 0
+    for(i in len - 1 downTo 0) {
+        if (locked[i] == '0')
+            total++
+        else {
+            if (s[i] == '(')
+                left++
+            else
+                right++
+        }
+        if (right + total < left)
+            ans = false
+    }
     return ans
 }
 
